8f2de24c4d07bf9484f5feed4859df503028413d
liujianling <1714538535@qq.com>
2023-05-07 21:56:25 +0800

    学习git

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div>
+            <h1>fsef</h1>
+            <el-button>测试</el-button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "HelloWorld",
+    props: {
+        msg: String,
+    },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
